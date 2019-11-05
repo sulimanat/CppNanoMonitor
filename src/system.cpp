6f3693206619456983fcc8a097970e9357472729
sulimanat <s.altwayan@hotmail.com>
2019-11-05 13:40:00 +0000

    Final

--- src/system.cpp
+++ src/system.cpp
@@ -14,7 +14,8 @@ using std::string;
 using std::vector;
 
 // TODO: Return the system's CPU
-Processor& System::Cpu() { return cpu_; }
+Processor& System::Cpu() { 
+  return cpu_; }
 
 // TODO: Return a container composed of the system's processes
 vector<Process>& System::Processes() { 
